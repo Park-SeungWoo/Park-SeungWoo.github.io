074a4bb3efc6ec95128430b6a343fdeff623886e
Park-SeungWoo <54872527+Park-SeungWoo@users.noreply.github.com>
2025-12-24 07:18:44 +0000

    Deploying to gh-pages from @ Park-SeungWoo/Park-SeungWoo.github.io@d2b5a313ffe8f1d7702fa24ebdc40241494433fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,70 +30,55 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
+        },{id: "post-回顧-2025",
+        
+          title: "回顧 2025",
+        
+        description: "Reflection of this year 2025",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/reflection2025/";
+          
+        },
+      },{id: "books-유닉스의-탄생",
+          title: '유닉스의 탄생',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+              window.location.href = "/books/the_birth_of_unix/";
+            },},{id: "books-큰-뜻을-품은-자여-왜-그-자리에-머물러-있는가",
+          title: '큰 뜻을 품은 자여, 왜 그 자리에 머물러 있는가',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "Books",handler: () => {
+              window.location.href = "/books/turing_s_vision%20copy/";
+            },},{id: "books-turing-39-s-vision-the-birth-of-computer-science",
+          title: 'Turing&amp;#39;s vision - The birth of Computer Science',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "Books",handler: () => {
+              window.location.href = "/books/turing_s_vision/";
+            },},{id: "news-beginning-of-this-blog-smile-rocket",
+          title: 'Beginning of this blog!! :smile: :rocket:',
           description: "",
           section: "News",},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%6D%64%32%34%37%31%30@%67.%73%6B%6B%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Park-SeungWoo", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://instagram.com/win_wooooooo", "_blank");
         },
       },{
       id: 'light-theme',
